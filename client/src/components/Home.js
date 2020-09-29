9065604d4812ec3f5bb91fb85fccb2246964fc12
ashtonlim <ashtonlim22@gmail.com>
2020-09-29 21:19:19 +0800

    removed useless

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,8 @@
-import React, { useEffect } from 'react'
-import Table from './subComponents/Table'
-// import AreaChart from './subComponents/AreaChart'
-import MainLayout from './layouts/MainLayout'
-
-// const res = await fetch(`/users`, { method: 'GET' })
-// const API = process.env.BASEAPI
-
-const API = 'http://localhost:8000'
+import React from "react";
+import Table from "./subComponents/Table";
+import MainLayout from "./layouts/MainLayout";
 
 function Home() {
-  useEffect(() => {
-    const apiCall = async () => {
-      const res = await fetch(`${API}/users`, { method: 'GET' })
-      const data = await res.json()
-      console.log(data)
-    }
-    // fetch(`/users`, { method: 'GET' })
-    //   .then((res) => {
-    //     console.log(res.json())
-    //   })
-    //   .catch((err) => console.log(err))
-    apiCall()
-  })
   return (
     <MainLayout>
       <section>
@@ -29,7 +10,7 @@ function Home() {
         <Table />
       </section>
     </MainLayout>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,52 +1,46 @@
-import React, { useState } from 'react'
-import { Redirect, Link } from 'react-router-dom'
-import { Form, Input, Button, Checkbox } from 'antd'
+import React, { useState } from "react";
+import { Redirect, Link } from "react-router-dom";
+import { Form, Input, Button, Checkbox } from "antd";
 
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
-import MainLayout from './layouts/MainLayout'
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-}
+import MainLayout from "./layouts/MainLayout";
 
 export default () => {
   const [vals, setVals] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
     usernameReqPF: false,
     pwdReqPF: false,
-    errMsg: '',
+    errMsg: "",
     redirect: null,
-    loginErrMsg: '',
-  })
+    loginErrMsg: "",
+  });
 
-  const [form] = Form.useForm()
+  const [form] = Form.useForm();
 
   const onLogin = () => {
-    console.log('login')
-  }
+    console.log("login");
+  };
 
   function onChange(e) {
-    console.log(`checked = ${e.target.checked}`)
+    console.log(`checked = ${e.target.checked}`);
   }
 
   const handleInput = (e) => {
-    let { name, val } = e.target
-    setVals({ ...vals, [name]: val })
-  }
+    let { name, val } = e.target;
+    setVals({ ...vals, [name]: val });
+  };
   const onFinish = (values) => {
-    console.log('Success:', values)
-  }
+    console.log("Success:", values);
+  };
 
   const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo)
-  }
+    console.log("Failed:", errorInfo);
+  };
 
-  // console.log(this.props);
   if (false) {
-    return <Redirect to={this.state.redirect} />
+    return <Redirect to={this.state.redirect} />;
   } else
     return (
       <MainLayout>
@@ -55,7 +49,6 @@ export default () => {
             <div className="card">
               <h1>Login</h1>
               <Form
-                // {...formItemLayout}
                 layout="vertical"
                 form={form}
                 initialValues={{
@@ -72,7 +65,7 @@ export default () => {
                   rules={[
                     {
                       required: true,
-                      message: 'Please input your username!',
+                      message: "Please input your username!",
                     },
                   ]}
                 >
@@ -90,7 +83,7 @@ export default () => {
                   rules={[
                     {
                       required: true,
-                      message: 'Please input your password!',
+                      message: "Please input your password!",
                     },
                   ]}
                 >
@@ -116,37 +109,9 @@ export default () => {
                   </Button>
                 </Form.Item>
               </Form>
-              <Button>
-                <Link to="/markets">GO TO MARKETS</Link>
-              </Button>
-
-              {/* <div>
-                <input className="inp-cbx" id="noon" type="checkbox" />
-                <label className="cbx" htmlFor="noon">
-                  <span>
-                    <svg width="12px" height="10px">
-                      <use xlinkHref="#check"></use>
-                    </svg>
-                  </span>
-                  <span>Remember me</span>
-                </label>
-                <svg className="inline-svg">
-                  <symbol id="check" viewBox="0 0 12 10">
-                    <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                  </symbol>
-                </svg>
-              </div>
-
-              <div className="control mtb20">
-                 <p className="help is-danger">{this.props.users.loginErrMsg}</p>
-                <p className="help is-danger">{this.state.loginErrMsg}</p> 
-                <button onClick={onLogin} className="button is-primary">
-                  Login
-                </button> */}
-              {/* </div> */}
             </div>
           </section>
         </div>
       </MainLayout>
-    )
-}
+    );
+};
